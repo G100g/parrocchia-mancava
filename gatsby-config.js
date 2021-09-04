02794fcdf11f728a5f1ce94d64283ae864929b9c
G100g <gigetto@gmail.com>
2021-09-04 17:07:05 +0200

    chore: revert disabled plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
-var { createProxyMiddleware} = require("http-proxy-middleware");
+var { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = {
     siteMetadata: {
         title: "Mancava la Rete",
         description:
-            "Parrocchie di Manzolino, Cavazzona, Panzano, Riolo, Recovato e Rastellino"
+            "Parrocchie di Manzolino, Cavazzona, Panzano, Riolo, Recovato e Rastellino",
     },
     plugins: [
         "gatsby-plugin-react-helmet",
@@ -14,22 +14,22 @@ module.exports = {
             resolve: "gatsby-source-filesystem",
             options: {
                 path: `${__dirname}/static/img`,
-                name: "uploads"
-            }
+                name: "uploads",
+            },
         },
         {
             resolve: "gatsby-source-filesystem",
             options: {
                 path: `${__dirname}/src/pages`,
-                name: "pages"
-            }
+                name: "pages",
+            },
         },
         {
             resolve: "gatsby-source-filesystem",
             options: {
                 path: `${__dirname}/src/img`,
-                name: "images"
-            }
+                name: "images",
+            },
         },
         "gatsby-plugin-sharp",
         "gatsby-transformer-sharp",
@@ -40,8 +40,8 @@ module.exports = {
                     {
                         resolve: "gatsby-remark-relative-images",
                         options: {
-                            name: "uploads"
-                        }
+                            name: "uploads",
+                        },
                     },
                     {
                         resolve: "gatsby-remark-images",
@@ -49,24 +49,24 @@ module.exports = {
                             // It's important to specify the maxWidth (in pixels) of
                             // the content container as this plugin uses this as the
                             // base for generating different widths of each image.
-                            maxWidth: 2048
-                        }
+                            maxWidth: 2048,
+                        },
                     },
                     {
                         resolve: "gatsby-remark-copy-linked-files",
                         options: {
-                            destinationDir: "static"
-                        }
-                    }
-                ]
-            }
+                            destinationDir: "static",
+                        },
+                    },
+                ],
+            },
         },
-        // {
-        //     resolve: "gatsby-plugin-netlify-cms",
-        //     options: {
-        //         modulePath: `${__dirname}/src/cms/cms.js`
-        //     }
-        // },
+        {
+            resolve: "gatsby-plugin-netlify-cms",
+            options: {
+                modulePath: `${__dirname}/src/cms/cms.js`,
+            },
+        },
         // {
         //   resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
         //   options: {
@@ -74,19 +74,19 @@ module.exports = {
         //     purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
         //   },
         // }, // must be after other CSS plugins
-        // "gatsby-plugin-netlify" // make sure to keep it last in the array
+        "gatsby-plugin-netlify", // make sure to keep it last in the array
     ],
     // for avoiding CORS while developing Netlify Functions locally
     // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
-    developMiddleware: app => {
+    developMiddleware: (app) => {
         app.use(
             "/.netlify/functions/",
             createProxyMiddleware({
                 target: "http://localhost:9000",
                 pathRewrite: {
-                    "/.netlify/functions/": ""
-                }
+                    "/.netlify/functions/": "",
+                },
             })
         );
-    }
+    },
 };

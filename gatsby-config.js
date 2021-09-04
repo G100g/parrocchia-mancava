d0d46cc52f07598c73a260db79943879b97dae34
G100g <gigetto@gmail.com>
2021-09-04 16:42:58 +0200

    chore: fix eslint errors

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
-var proxy = require("http-proxy-middleware");
+var { createProxyMiddleware} = require("http-proxy-middleware");
 
 module.exports = {
     siteMetadata: {
-        title: "Parrocchie di Manzolino e Cavazzona",
-        description: ""
+        title: "Mancava la Rete",
+        description:
+            "Parrocchie di Manzolino, Cavazzona, Panzano, Riolo, Recovato e Rastellino"
     },
     plugins: [
         "gatsby-plugin-react-helmet",
@@ -60,12 +61,12 @@ module.exports = {
                 ]
             }
         },
-        {
-            resolve: "gatsby-plugin-netlify-cms",
-            options: {
-                modulePath: `${__dirname}/src/cms/cms.js`
-            }
-        },
+        // {
+        //     resolve: "gatsby-plugin-netlify-cms",
+        //     options: {
+        //         modulePath: `${__dirname}/src/cms/cms.js`
+        //     }
+        // },
         // {
         //   resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
         //   options: {
@@ -73,14 +74,14 @@ module.exports = {
         //     purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
         //   },
         // }, // must be after other CSS plugins
-        "gatsby-plugin-netlify" // make sure to keep it last in the array
+        // "gatsby-plugin-netlify" // make sure to keep it last in the array
     ],
     // for avoiding CORS while developing Netlify Functions locally
     // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
     developMiddleware: app => {
         app.use(
             "/.netlify/functions/",
-            proxy({
+            createProxyMiddleware({
                 target: "http://localhost:9000",
                 pathRewrite: {
                     "/.netlify/functions/": ""
